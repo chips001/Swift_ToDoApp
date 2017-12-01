afd5d14d9edf4a99c7147f25dc55a4ed8153ac1c
hideki.ichiki <hideki.ichiki@mkt-apps.com>
2017-12-01 22:44:27 +0900

    Revert "TestCommit"
    This reverts commit 8f22b11937a6623079cd694410937838b6f062e4.

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -135,6 +135,4 @@ class TaskCell: UITableViewCell{
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-    
-    //コミットテスト
 }
